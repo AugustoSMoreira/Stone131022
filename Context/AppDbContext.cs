e98bfe5ac320727a426cdf03679d8fe4027919c6
Augusto Moreira <gustoux@gmail.com>
2022-10-13 10:55:44 -0300

    Scafold Commit

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Stone131022.Models;
+
+namespace Stone131022.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Jogador> GetJogadores { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(
+                    "Server=localhost;initial catalog=CRUD_MYSQL;uid=root;pwd=admin",
+                        Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.30-mysql"));
+        }
+    }
+}
---
+++ Controllers/JogadoresController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Stone131022.Context;
+using Stone131022.Models;
+
+namespace Stone131022.Controllers
+{
+    public class JogadoresController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public JogadoresController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Jogadores
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.GetJogadores.ToListAsync());
+        }
+
+        // GET: Jogadores/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.GetJogadores == null)
+            {
+                return NotFound();
+            }
+
+            var jogador = await _context.GetJogadores
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (jogador == null)
+            {
+                return NotFound();
+            }
+
+            return View(jogador);
+        }
+
+        // GET: Jogadores/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Jogadores/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nome,Posicao,Idade,MedalhaId")] Jogador jogador)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(jogador);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(jogador);
+        }
+
+        // GET: Jogadores/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.GetJogadores == null)
+            {
+                return NotFound();
+            }
+
+            var jogador = await _context.GetJogadores.FindAsync(id);
+            if (jogador == null)
+            {
+                return NotFound();
+            }
+            return View(jogador);
+        }
+
+        // POST: Jogadores/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,Posicao,Idade,MedalhaId")] Jogador jogador)
+        {
+            if (id != jogador.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(jogador);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!JogadorExists(jogador.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(jogador);
+        }
+
+        // GET: Jogadores/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.GetJogadores == null)
+            {
+                return NotFound();
+            }
+
+            var jogador = await _context.GetJogadores
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (jogador == null)
+            {
+                return NotFound();
+            }
+
+            return View(jogador);
+        }
+
+        // POST: Jogadores/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.GetJogadores == null)
+            {
+                return Problem("Entity set 'AppDbContext.GetJogadores'  is null.");
+            }
+            var jogador = await _context.GetJogadores.FindAsync(id);
+            if (jogador != null)
+            {
+                _context.GetJogadores.Remove(jogador);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool JogadorExists(int id)
+        {
+          return _context.GetJogadores.Any(e => e.Id == id);
+        }
+    }
+}
